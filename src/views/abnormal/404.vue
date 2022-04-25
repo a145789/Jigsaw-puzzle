9ba9b52eea1ac5bd1953df2335f1612339048220
a145789 <2091927351@qq.com>
2022-04-25 19:22:29 +0800

    feat: rescript

--- src/views/abnormal/404.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    无此页面，请返回...
-  </div>
-</template>
--- src/views/main.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="a-block">
-    <router-link :to="{name:'jigsaw-puzzle'}">1.拼图</router-link>
-    <router-link :to="{name:'test-main'}">2.vue API 测试</router-link>
-  </div>
-</template>
