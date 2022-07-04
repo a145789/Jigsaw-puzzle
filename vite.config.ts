8fc60a24adddc24f4e87a53de78dae5172040d32
a145789 <2091927351@qq.com>
2022-07-04 21:10:23 +0800

    chore: update dep

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from "vite"
-import reactRefresh from "@vitejs/plugin-react-refresh"
-import createReScriptPlugin from "@jihchi/vite-plugin-rescript"
+import react from '@vitejs/plugin-react'
+import createReScriptPlugin from '@jihchi/vite-plugin-rescript'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), createReScriptPlugin()],
-  base: "./",
+  plugins: [react(), createReScriptPlugin()],
+  base: './'
 })
